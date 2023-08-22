3b2f84d03ce99e601918fb1f779ad09c74c4b6b7
岡田祐人 <>
2023-08-22 18:40:41 +0900

    初期データの登録

--- AtlasManagementSystem/database/seeds/UsersTableSeeder.php
+++ AtlasManagementSystem/database/seeds/UsersTableSeeder.php
@@ -12,6 +12,17 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users')->insert([
+            ['over_name' => 'テスト',
+            'under_name' => 'ユーザー',
+            'over_name_kana' => 'テスト',
+            'under_name_kana' => 'ユーザー',
+            'mail_address' => 'test@test',
+            'sex' => 1,
+            'birth_day' => '1993-11-01',
+            'role' => 4,
+            'password' => 'test1101',
+            ]
+        ]);
     }
 }
