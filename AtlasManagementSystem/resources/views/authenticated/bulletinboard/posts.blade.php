0df348c0e739769f32f142afc5c11126dda445f6
岡田祐人 <>
2023-11-04 16:14:20 +0900

    レイアウトの調整　投稿一覧表示画面　投稿一覧

--- AtlasManagementSystem/resources/views/authenticated/bulletinboard/posts.blade.php
+++ AtlasManagementSystem/resources/views/authenticated/bulletinboard/posts.blade.php
@@ -6,21 +6,25 @@
     <p class="w-75 m-auto">投稿一覧</p>
     @foreach($posts as $post)
     <div class="post_area border w-75 m-auto p-3">
-      <p><span>{{ $post->user->over_name }}</span><span class="ml-3">{{ $post->user->under_name }}</span>さん</p>
-      <p><a href="{{ route('post.detail', ['id' => $post->id]) }}">{{ $post->post_title }}</a></p>
+      <p style="color: #999; font-weight: bold; font-size:15px;"><span>{{ $post->user->over_name }}</span><span class="ml-3">{{ $post->user->under_name }}</span>さん</p>
+      <p ><a href="{{ route('post.detail', ['id' => $post->id]) }}" style="color: #000; font-weight:bold;">{{ $post->post_title }}</a></p>
       <div class="post_bottom_area d-flex">
-        <div class="d-flex post_status">
-          @foreach($post->subCategories as $subCategory)
-          <span class="category_btn">{{ $subCategory->sub_category }}</span>
-          @endforeach
-          <div class="mr-5">
-            <i class="fa fa-comment"></i><span class="">{{ $post->commentCounts($post->id)->count() }}</span>
+        <div class="d-flex post_status w-100 justify-content-between">
+          <div class="d-flex">
+            <div class="mr-auto">
+              @foreach($post->subCategories as $subCategory)
+              <span class="category_btn">{{ $subCategory->sub_category }}</span>
+              @endforeach
+            </div>
           </div>
-          <div>
+          <div class="ml-auto">
+              <i class="fa fa-comment"></i><span class="ml-2">{{ $post->commentCounts($post->id)->count() }}</span>
+          </div>
+          <div class="d-flex ml-5">
             @if(Auth::user()->is_Like($post->id))
-            <p class="m-0"><i class="fas fa-heart un_like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{ $post->id }}">{{ $post->likes()->count() }}</span></p>
+            <p class="m-0"><i class="fas fa-heart un_like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{ $post->id }} ml-2">{{ $post->likes()->count() }}</span></p>
             @else
-            <p class="m-0"><i class="fas fa-heart like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{ $post->id }}">{{ $post->likes()->count() }}</span></p>
+            <p class="m-0"><i class="fas fa-heart like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{ $post->id }} ml-2">{{ $post->likes()->count() }}</span></p>
             @endif
           </div>
         </div>
