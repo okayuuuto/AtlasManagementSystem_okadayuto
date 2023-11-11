caeaadee9ec0551c7957d7c88d45f41ed32a39bd
岡田祐人 <>
2023-11-12 02:35:17 +0900

    レイアウトの調整　スクール予約　予約確認画面

--- AtlasManagementSystem/app/Calendars/Admin/CalendarWeekDay.php
+++ AtlasManagementSystem/app/Calendars/Admin/CalendarWeekDay.php
@@ -16,7 +16,7 @@ class CalendarWeekDay{
   }
 
   function render(){
-    return '<p class="day">' . $this->carbon->format("j") . '日</p>';
+    return '<p class="day mb-0">' . $this->carbon->format("j") . '日</p>';
   }
 
   function everyDay(){
@@ -29,15 +29,15 @@ class CalendarWeekDay{
     $two_part = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '2')->first();
     $three_part = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '3')->first();
 
-    $html[] = '<div class="text-left">';
+    $html[] = '<div class="text-center">';
     if($one_part){
-      $html[] = '<p class="day_part m-0 pt-1"><a href="' .route('calendar.admin.detail', ['date' => $ymd, 'part' => '1']) . '">1部</a>　' .count($one_part->users) . '</p>';
+      $html[] = '<p class="day_part d-flex m-0 pt-1"><a href="' .route('calendar.admin.detail', ['date' => $ymd, 'part' => '1']) . '">1部</a>　' .count($one_part->users) . '</p>';
     }
     if($two_part){
-      $html[] = '<p class="day_part m-0 pt-1"><a href="' .route('calendar.admin.detail', ['date' => $ymd, 'part' => '2']) . '">2部</a>　' .count($two_part->users) . '</p>';
+      $html[] = '<p class="day_part d-flex m-0 pt-1"><a href="' .route('calendar.admin.detail', ['date' => $ymd, 'part' => '2']) . '">2部</a>　' .count($two_part->users) . '</p>';
     }
     if($three_part){
-      $html[] = '<p class="day_part m-0 pt-1"><a href="' .route('calendar.admin.detail', ['date' => $ymd, 'part' => '3']) . '">3部</a>　' .count($three_part->users) . '</p>';
+      $html[] = '<p class="day_part d-flex m-0 pt-1"><a href="' .route('calendar.admin.detail', ['date' => $ymd, 'part' => '3']) . '">3部</a>　' .count($three_part->users) . '</p>';
     }
     $html[] = '</div>';
 
--- AtlasManagementSystem/resources/views/authenticated/calendar/admin/calendar.blade.php
+++ AtlasManagementSystem/resources/views/authenticated/calendar/admin/calendar.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.sidebar')
 
 @section('content')
-<div class="w-75 m-auto">
-  <div class="w-100">
-    <p>{{ $calendar->getTitle() }}</p>
+<div class="w-100 m-auto pt-5 pb-3">
+  <div class="w-75 border m-auto pt-5" style="border-radius:10px; background:#FFF; box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);">
+    <p class="text-center">{{ $calendar->getTitle() }}</p>
+    <div class="m-auto" style="width:90%;">
     <p>{!! $calendar->render() !!}</p>
+    </div>
   </div>
 </div>
 @endsection
